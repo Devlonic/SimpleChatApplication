7e23a30ee86cffd99d483fcf3e874161186fb9f0
Devloner <tminecraftben@gmail.com>
2024-07-12 16:25:04 +0300

    Fix test

--- SimpleChatApplication.Tests/ChatRequests_Tests.cs
+++ SimpleChatApplication.Tests/ChatRequests_Tests.cs
@@ -1,13 +1,57 @@
-﻿using SimpleChatApplication.BLL.CQRS.ChatRooms.Commands;
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using SimpleChatApplication.BLL.CQRS.ChatRooms.Commands;
 using SimpleChatApplication.BLL.CQRS.Users.Commands;
 using SimpleChatApplication.BLL.Exceptions;
+using SimpleChatApplication.BLL.Services;
+using SimpleChatApplication.BLL;
+using SimpleChatApplication.DAL;
+using SimpleChatApplication.DAL.Data.Contexts;
 using Xunit;
+using Microsoft.EntityFrameworkCore;
 
 namespace SimpleChatApplication.Tests {
-    public class ChatRequests_Tests : TestBase {
+    public class ChatRequests_Tests {
         [Fact]
         public async Task ShouldThrowsForbidenException_WhenUserTriesToDeleteForeignChat() {
             /// Arrange
+
+            // create empty services vault
+            var Services = new ServiceCollection();
+
+            // setup configuration
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.Tests.json");
+            var configuration = configurationBuilder.Build();
+
+            // change default connection string name 
+            // to prevent parallel multi-test access to same database instance
+            configuration["ConnectionStrings:default"] = string.Format(configuration["ConnectionStrings:default"] ?? "", Guid.NewGuid());
+
+            // mock Messenger
+            var mock = new Mock<IRealTimeMessenger>();
+            mock.Setup(a => a.SendMessageToAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+
+            mock.Setup(a => a.SendMessageToAsync(It.IsAny<IList<string>>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+
+            // setup DAL, BLL services
+            Services.AddBusinessLogicLayerServices(configuration);
+            Services.AddDataAccessLayerServices(configuration);
+            Services.AddLogging();
+            Services.AddSingleton<IRealTimeMessenger>(mock.Object);
+            var ServiceProvider = Services.BuildServiceProvider();
+
+            // recreate database
+            var database = ServiceProvider.GetRequiredService<ChatApplicationDbContext>();
+            await database.Database.EnsureDeletedAsync();
+            await database.Database.MigrateAsync();
+
+            // get mediator instance
+            var Mediator = ServiceProvider.GetRequiredService<IMediator>();
 
             /// Act
 
